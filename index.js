f1b3b50be645e9234d8ea122d8441fa669230a1e
Jesús Monge <jesus_jaml@outlook.com>
2022-03-14 00:55:41 -0500

    📲 Project image slider

--- index.js
+++ index.js
@@ -1,63 +1,36 @@
-window.onload = () => {
-  document.querySelector(".arrow-right").addEventListener("click", clickRight);
-  document.querySelector(".arrow-left").addEventListener("click", clickLeft);
-  document
-    .querySelector(".send-button")
-    .addEventListener("click", showNotification);
-  document.querySelectorAll(".project").forEach(element => {
-    element.addEventListener("click", e => openModal(e));
-  });
-  document.body.addEventListener("click", e => closeModal(e));
-};
+let counter = 1
+let $slider = document.getElementsByClassName('slider')[0]
+console.log($slider)
 
-/** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
-function clickRight() {
-  const currentLeft = parseInt(
-    getComputedStyle(document.querySelector(".project-container")).left,
-    10
-  );
-  if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
-    return;
+// function consoleLog() {
+//   console.log('clicked')
+// }
+
+const intervalo = setInterval(() => {
+  document.getElementById('radio' + counter).checked = true
+  counter++
+  if (counter > 4) {
+    counter = 1
   }
-  let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
-  document.querySelector(".project-container").style.left = `${newValue}px`;
-}
+}, 5000)
 
-/** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
-function clickLeft() {
-  const currentLeft = parseInt(
-    getComputedStyle(document.querySelector(".project-container")).left,
-    10
-  );
-  if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
-    return;
-  }
-  let newValue = currentLeft + 270;
-  document.querySelector(".project-container").style.left = `${newValue}px`;
-}
+$slider.addEventListener('mouseenter', () => {
+  clearInterval(intervalo)
+})
 
-/** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
-function showNotification() {
-  document.querySelector(".notification").style.display = "flex";
-  setTimeout(function() {
-    document.querySelector(".notification").style.display = "none";
-  }, 3000);
-}
+// $slider.addEventListener('mouseleave', () => {
+//   const intervalo = setInterval(() => {
+//     document.getElementById('radio' + counter).checked = true
+//     counter++
+//     if (counter > 4) {
+//       counter = 1
+//     }
+//   }, 2000)
+// })
 
-/** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
-function openModal(e) {
-  document.querySelector(".modal-container").style.display = "flex";
-}
-
-/** Esta funcion se llama para cerrar el modal */
-function closeModal(e) {
-  // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
-  if (
-    e.target.className.includes("project") ||
-    e.target.className === "modal"
-  ) {
-    return;
-  } else {
-    document.querySelector(".modal-container").style.display = "none";
-  }
-}
+// $slider.addEventListener('mouseenter', (e) => {
+//   $slider.style.border = '5px dotted orange'
+// })
+// $slider.addEventListener('mouseleave', (e) => {
+//   $slider.style.border = '5px solid red'
+// })
